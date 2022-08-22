f4eca6e91c01548dd1533e5223021dcd9b7c4126
Saburov <the.saburchik@gmail.com>
2022-08-22 19:04:06 +0700

    added flyweight

---
+++ design-patterns/src/Flyweight/JavaDeveloper.java
@@ -0,0 +1,8 @@
+package Flyweight;
+
+public class JavaDeveloper implements Developer {
+	@Override
+	public void writeCode() {
+		System.out.println("Java developer writes java code...");
+	}
+}
---
+++ design-patterns/src/Flyweight/ProjectRunner.java
@@ -0,0 +1,25 @@
+package Flyweight;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProjectRunner {
+
+	public static void main(String[] args) {
+		DeveloperFactory developerFactory = new DeveloperFactory();
+		
+		List<Developer> developers = new ArrayList<>();
+		
+		developers.add(developerFactory.getDeveloperBySpecialty("java"));
+		developers.add(developerFactory.getDeveloperBySpecialty("java"));
+		developers.add(developerFactory.getDeveloperBySpecialty("java"));
+		developers.add(developerFactory.getDeveloperBySpecialty("java"));
+		developers.add(developerFactory.getDeveloperBySpecialty("c++"));
+		developers.add(developerFactory.getDeveloperBySpecialty("c++"));
+		
+		for(Developer developer: developers) {
+			developer.writeCode();
+		}
+	}
+
+}
